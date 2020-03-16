82c1a2016b9ed8db0e6c03daf6f59089e3d06d50
renderhaf <dvir.akerman@gmail.com>
2020-03-16 11:49:20 +0200

    Added datavalidator - spoil checker and adder

---
+++ DataValidator.py
@@ -0,0 +1,38 @@
+import time
+
+'''
+This is a dictionary that defines the amount of time every item has until its spoiled
+Y - Yearly Candle
+M - Monthly Candle
+W - Weekly Candle
+Q - Quote
+'''
+spoilTimes = {"Y": 30, "M": 30, "W": 10, "Q": 0.5}
+expirationDateKey = "expirationDate"
+spoilTimeConversion = 3600
+
+'''
+Returns whether the data is expired or not
+'''
+def isExpired(data : dict):
+    return int(data[expirationDateKey]) <= int(time.time())
+
+'''
+Mutates the dict so that it has a correct expiration date
+'''
+def putExpirationDate(data: dict) -> None:
+    try:
+        expirationDiff = spoilTimes[data["timeframe"]] * spoilTimeConversion
+    except KeyError:
+        print("The data does not have a timeframe, cannot put an expiration date on it.")
+        return
+    data[expirationDateKey] = int(time.time()) + expirationDiff
+
+
+def test():
+    data = {"prices": [0,1,2,3,4], "timeframe": "Y"}
+    putExpirationDate(data)
+    print(time.ctime(data[expirationDateKey]), isExpired(data))
+
+if __name__ == "__main__":
+    test()
