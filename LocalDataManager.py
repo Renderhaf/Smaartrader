95c76f864cf5e03e5c8017eb93386142a7e3d08b
renderhaf <dvir.akerman@gmail.com>
2020-03-17 01:37:27 +0200

    Small fixes for the pull request

--- LocalDataManager.py
+++ LocalDataManager.py
@@ -1,14 +1,14 @@
 import json
-import DataValidator
+import DataValidator as DV
 import time
 
 
 documentName = "localStorage.json"
 
 def putData(symbol, data: dict) -> None:   
-    '''
+    """
     Puts Data in the local storage if its not already there
-    '''
+    """
     try:
         with open(documentName, "r") as file:
             readFile = file.read()
@@ -23,31 +23,31 @@ def putData(symbol, data: dict) -> None:
     if symbol in currentData.keys():
         #Check if the data is already in the list
         for document in currentData[symbol]:
-            #To check if no appending is requiered, check if the type and timeframe match, and make sure its not expierd
-            if document["type"] == data["type"] and document["timeframe"] == data["timeframe"] and not DataValidator.isExpired(document):
+            #To check if no appending is required, check if the type and timeframe match, and make sure its not expired
+            if document["type"] == data["type"] and document["timeframe"] == data["timeframe"] and not DV.isExpired(document):
                 return
     else:
         currentData[symbol] = []
 
     #Puts an expiration date on the data
-    DataValidator.putExpirationDate(data)
+    DV.putExpirationDate(data)
 
     with open(documentName, "w") as file:       
         currentData[symbol].append(data)
         file.write(json.dumps(currentData))
 
 def getData(symbol, datatype: str, timeframe: str) -> dict:
-    '''
+    """
     Gets data from the local storage\n
-    If the data is not existent in the local storage, the function will return {}
-    '''
+    If the data does not exist in the local storage, the function will return {}
+    """
     returndata = {}
     try:
         with open(documentName, "r") as file:
             currentData = json.loads(file.read())
             if symbol in currentData.keys():
                 for document in currentData[symbol]:
-                    if document["type"] == datatype and document["timeframe"] == timeframe and not DataValidator.isExpired(document):
+                    if document["type"] == datatype and document["timeframe"] == timeframe and not DV.isExpired(document):
                         returndata = document
     except FileNotFoundError:
         print("Local storge file not found")
