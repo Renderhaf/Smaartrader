2d81dcd646793968e5e65715ccb0ccc3be1304f3
shilodaum <shilodaum@gmail.com>
2020-03-17 00:17:24 +0200

    updated imports

--- InfoManager.py
+++ InfoManager.py
@@ -8,31 +8,58 @@ class InfoManager():
     resolutionToCount={'D':365,60:720,30:336}
     resolutionToTime={'D':'Y',60:'M',30:'W'}
     
+    """get stock candle data form stock manager
+    
+    Returns:
+        [dict] -- [candle]
+    """
     @staticmethod
     def getStockCandle(symbol,resolution='D',count=365)->dict:
         return SM.StockManager.getCandle(symbol,resolution,count)
     
+    """get stocks current quote from stock manager
+    
+    Returns:
+        [dict] -- [cuurent quote data]
+    """
     @staticmethod
     def getStockQuote(symbol)->dict:
         return SM.StockManager.getQuote(symbol)
     
+    """general get candle, checks where to get it from
+    
+    Returns:
+        [dict] -- [candle stock data]
+    """
     @staticmethod
     def getCandle(symbol,resolution='D')->dict:
         #one of the options in DB
         if resolution in InfoManager.resolutionToTime.keys():
-            if DM.DatabaseManager.updated()==True:
+            #updated DB
+            if DM.DatabaseManager.updated():
                 return InfoManager.getDBCandle()
             else:
+                #not updated,update and get from stock manger
                 return InfoManager.updateDBFromSM(symbol,resolution)
         else:
+            #not a regular DB option, get from stock manager
             return InfoManager.getStockCandle(symbol,resolution,InfoManager.resolutionToCount[resolution])
 
-
+    """get candle from Database
+    
+    Returns:
+        [dict] -- [candle of stock]
+    """
     @staticmethod
     def getDBCandle(symbol,resolution='D')->dict:
         oCandle = DM.DatabaseManager.getData(symbol,resolution)
         return dict(oCandle)
     
+    """update database and get data from stock manager
+    
+    Returns:
+        [dict] -- [candle]
+    """
     #ToDo make function threded
     @staticmethod
     def updateDBFromSM(symbol,resolution='D')->dict:
