1e90ed364e219079fc6c1c5a970e68014bca41c5
renderhaf <dvir.akerman@gmail.com>
2020-03-17 13:07:02 +0200

    Database Manager Works

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,19 +1,40 @@
 import pymongo
-
+import DataValidator as DV
 
 client = pymongo.MongoClient("mongodb+srv://StockManager:Manage123@maincluster-zlnck.mongodb.net/test?retryWrites=true&w=majority")
-database =client.get_database("StocksInfo")
-#TODO find out resolutions meaning
+database = client.get_database("StocksInfo")
 
-isUpdated=False
+def storeData(symbol, data):
+    timeframe = data["timeframe"]
+    currentStoredData = getData(symbol, timeframe)
+    #If the data is expired, remove it
+    if DV.isExpired(currentStoredData):
+        removeData(symbol, timeframe)
+        currentStoredData = {}
+    #If the data is not already there store it
+    if currentStoredData == {}:
+        #Store the data
+        collection = database.get_collection(symbol)
+        newdata = data
+        DV.putExpirationDate(newdata)
+        collection.insert_one(newdata)
 
+def removeData(symbol, timeframe):
+    collection = database.get_collection(symbol)
+    collection.delete_one({"timeframe": timeframe})
 
-def updated():
-    return False
-    #TODO write real updated checker
+def getData(symbol, timeframe):
+    collection = database.get_collection(symbol)
+    data = list(collection.find({"timeframe": timeframe}))
+    if len(data) == 0:
+        return {}
+    else:
+        return data[0]
 
-def storeData(candle,symbol,resolution):
-    pass
+def test():
+    # storeData("GOOGL", {"prices": [1,2,3,4], "timeframe": "Y"})
+    # removeData("GOOGL", "Y")
+    print(getData("GOOGL", "Y"))
 
-def getData(symbol,resolution):
-    pass
+if __name__ == "__main__":
+    test()
