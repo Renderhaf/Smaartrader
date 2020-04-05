6ad9246bd5a81b4793d2f1f7516d259da1642bc4
renderhaf <dvir.akerman@gmail.com>
2020-04-06 00:55:01 +0300

    Fixed front-end merge issuses

--- server.py
+++ server.py
@@ -1,19 +1,16 @@
 import json
-import os
-import sys
-import time
-
 import requests
-from flask import Flask, make_response, render_template, request, jsonify
-
-#This is a module for Web Security, its currently used for sessions and CSRF protection
-import WebSecurity
-WebSecurity.resetStorage()
+import os
+import time
+from flask import Flask, render_template, request
+import json
+import time
+import sys
 
 #This adds the StockInfoPackage package to the path, so we can import from it
 sys.path.insert(1, './StockInfoPackage/')
-import InfoManager as IM
 
+import InfoManager as IM
 
 app = Flask(__name__)
 
@@ -25,63 +22,41 @@ with open("companies.json", "r") as file:
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
-    if request.method == "POST": #POST
-        if not WebSecurity.validatePOST(request):
-            return "Bad request. The server could not validate this POST request", 403 #Forbidden Request Status Code
-        
-        try: #These are put in a try except block in order to prevent crashing when bad post requests are sent
-            if request.form["type"] == "quote+candle":
-                if request.form["isMulti"] == 'true':
-                    symbolData = {}
-                    for symbol in json.loads(request.form["names"]):
-                        data = IM.getStockQuote(symbol)
-                        candleData = IM.getCandle(symbol, request.form["time"])
-                        data["prices"] = candleData["c"]
-                        data["dates"] = [time.ctime(t) for t in candleData["t"]]
-                        symbolData[symbol] = data
-                    return symbolData
-                else:
-                    data = IM.getStockQuote(request.form["name"])
-                    candleData = IM.getCandle(request.form["name"], request.form["time"])
+    if request.method == "POST":
+        if request.form["type"] == "quote+candle":
+            if request.form["isMulti"] == 'true':
+                symbolData = {}
+                for symbol in json.loads(request.form["names"]):
+                    data = IM.getStockQuote(symbol)
+                    candleData = IM.getCandle(symbol, request.form["time"])
                     data["prices"] = candleData["c"]
                     data["dates"] = [time.ctime(t) for t in candleData["t"]]
-                    return data
-
-            if request.form["type"] == "candle":
-                stockData = IM.getCandle(request.form["name"], request.form["time"])
-                prices = stockData["c"]
-                dates = [time.ctime(t) for t in stockData["t"]]
-                data = {"prices":prices, "dates":dates}
+                    symbolData[symbol] = data
+                return symbolData
+            else:
+                data = IM.getStockQuote(request.form["name"])
+                candleData = IM.getCandle(request.form["name"], request.form["time"])
+                data["prices"] = candleData["c"]
+                data["dates"] = [time.ctime(t) for t in candleData["t"]]
                 return data
 
-            if request.form["type"] == "quote":
-                return IM.getStockQuote(request.form["name"])
+        if request.form["type"] == "candle":
+            stockData = IM.getCandle(request.form["name"], request.form["time"])
+            prices = stockData["c"]
+            dates = [time.ctime(t) for t in stockData["t"]]
+            data = {"prices":prices, "dates":dates}
+            return data
 
-        except Exception as err:
-            print("Internal Server Error: ", err)
-            return "Internal Server Error", 500 #Internal Server Error Status Code
-            
-        except KeyError:
-            pass
+        if request.form["type"] == "quote":
+            return IM.getStockQuote(request.form["name"])
 
-        return "Bad request. The request is missing required keys", 400 #Bad Request Status Code
 
-        
-    else: #GET
+    else: 
         viewedStocks = companies
-        response = ""
         if len(viewedStocks) > 10:
-            response =  render_template("oneRequestIndex.html", stocks = viewedStocks)
+            return render_template("oneRequestIndex.html", stocks = viewedStocks)
         else:
-            response =  render_template("threadedRequestIndex.html", stocks = viewedStocks)
-
-        response = make_response(response)
-
-        #Decide whether this request needs a new sessionID
-        currentSessionID = request.cookies.get('sessionID', default="")
-        if not WebSecurity.isSessionStored(currentSessionID):
-            response.set_cookie("sessionID", WebSecurity.getNewSessionID())
-        return response
+            return render_template("threadedRequestIndex.html", stocks = viewedStocks)
 
 if __name__ == "__main__":
     port = os.environ.get('PORT') or 5000
