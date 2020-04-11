f567d645e4f0fec68ba61c5c26c41eeb2083f51d
renderhaf <dvir.akerman@gmail.com>
2020-04-12 00:21:36 +0300

    Added comments and var types

--- StockInfoPackage/StockAnalysisManager.py
+++ StockInfoPackage/StockAnalysisManager.py
@@ -25,19 +25,32 @@ def getWikiArticle(ticker: str)->str:
     return wikilink
 
 def getCurrentTrend(ticker: str, weeksBack:int=1):
+    '''
+    returns the current trend for @param weeksBack in precentage
+    '''
     stockData = IM.getCandle(ticker, timeframe="Y", quality="high").get("c")
     priceDiff = stockData[-1] - stockData[-weeksBack*7]
     diffPrecentage = (priceDiff / stockData[-weeksBack*7]) * 100
     return round(diffPrecentage,3)
 
-def getHistoricSMA(ticker, sampleSize=20):
+def getHistoricSMA(ticker: str, sampleSize: int=25):
+    '''
+    returns a list of a simple moving avarage (equal weighted)
+
+    * The list used should be shifted forward @param sampleSize times, since SMA is not calculated for those
+    '''
     stockData = IM.getCandle(ticker, timeframe="Y", quality="high").get("c")
     sma = []
     for i in range(0, len(stockData)-sampleSize):
         sma.append(sum(stockData[i : i + sampleSize])/sampleSize)
     return sma
 
-def getCurrentSMA(ticker, sampleSize=25, returnPrice=False):
+def getCurrentSMA(ticker: str, sampleSize: int=25, returnPrice: bool=False):
+    '''
+    returns the current simple avarage with @param sampleSize days
+
+    * if @param returnPrice is true, this will return a tuple containing the SMA and the current price (mostly used for comparison)
+    '''
     stockData = IM.getCandle(ticker, timeframe="Y", quality="high").get("c")[-sampleSize-1:]
     sma = 0
     for i in range(0, len(stockData)-sampleSize):
@@ -48,7 +61,13 @@ def getCurrentSMA(ticker, sampleSize=25, returnPrice=False):
     else:
         return round(sma,2)
 
-def getHistoricEMA(ticker, sampleSize=25, returnPrice=False):
+def getHistoricEMA(ticker: str, sampleSize: int=25, returnPrice: bool=False):
+    '''
+    returns a list of a exponential moving avarage (equal weighted)
+
+    * The list used should be shifted forward @param sampleSize times, since EMA is not calculated for those
+    * if @param returnPrice is true, this will return a tuple containing the SMA and the current price (mostly used for comparison)
+    '''
     stockData = IM.getCandle(ticker, timeframe="Y", quality="high").get("c")
     mult = 2 / (sampleSize+1)
     ema = []
@@ -64,12 +83,18 @@ def getHistoricEMA(ticker, sampleSize=25, returnPrice=False):
         return ema, stockData[-1]
     return ema
 
-def getCurrentEMA(ticker, sampleSize=20, returnPrice=False):
+def getCurrentEMA(ticker: str, sampleSize: int=20, returnPrice: bool=False):
+    '''
+    returns the current exponential avarage with @param sampleSize days
+    
+    * if @param returnPrice is true, this will return a tuple containing the EMA and the current price (mostly used for comparison)
+    '''
+
     if returnPrice:
         return round(getHistoricEMA(ticker, sampleSize, True)[-1],2)
     return round(getHistoricEMA(ticker, sampleSize)[-1],2)
 
-def main():
+def test():
     ticker = 'AAPL'
     normalStockY = IM.getCandle(ticker, timeframe="Y", quality="high").get("c")
     normalStockX = [x for x in range(len(normalStockY))]
@@ -88,5 +113,5 @@ def main():
     plt.show()
 
 if __name__ == "__main__":
-    main()
+    test()
 
