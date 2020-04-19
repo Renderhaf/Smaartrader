882ac6e2e6e708b0d7878ba91c47075b5b6a890d
renderhaf <dvir.akerman@gmail.com>
2020-04-19 13:33:24 +0300

    Added gui for catagories

--- StockInfoPackage/InfoManager.py
+++ StockInfoPackage/InfoManager.py
@@ -162,6 +162,8 @@ def getCandle(symbol,timeframe='Y', quality=default_quality, forceAPI=False)->di
     for state in candleStateOrder:
         data = state(symbol, timeframe,quality)
         if len(data.keys()) != 0:
+            if "_id" in data.keys():
+                data.pop("_id")
             return data
 
 def getQuote(symbol:str, forceAPI:bool=False)->dict:
@@ -180,6 +182,8 @@ def getQuote(symbol:str, forceAPI:bool=False)->dict:
     for state in quoteStateOrder:
         data = state(symbol)
         if len(data.keys()) != 0:
+            if "_id" in data.keys():
+                data.pop("_id")
             return data
 
 
--- views/Index.py
+++ views/Index.py
@@ -1,7 +1,7 @@
 from server import app
 from flask import request, render_template, redirect, make_response
 import time
-import sys 
+import sys
 import json
 
 import WebSecurity
@@ -13,31 +13,36 @@ import StockInfoPackage.InfoManager as IM
 
 with open("settings.json", "r") as file:
     data = json.loads(file.read())
-    categories:dict = data["categories"]
+    categories: dict = data["categories"]
     mobilePlatforms = data["mobilePlatforms"]
 
 '''
 Helper functions for getting inforamtion
 The function names are based on their type in the POST request
 '''
-def getCandlePlusQuote(stock: str, timeframe: str = "Y", quality:str = "high")->dict:
+
+
+def getCandlePlusQuote(stock: str, timeframe: str = "Y", quality: str = "high") -> dict:
     data = IM.getQuote(stock)
     candleData = IM.getCandle(stock, timeframe, quality)
     data["prices"] = candleData["c"]
     data["dates"] = [time.ctime(t) for t in candleData["t"]]
     return data
 
-def getCandle(stock: str, timeframe: str = "Y", quality:str = "high")->dict:
+
+def getCandle(stock: str, timeframe: str = "Y", quality: str = "high") -> dict:
     stockData = IM.getCandle(stock, timeframe, quality)
     prices = stockData["c"]
     dates = [time.ctime(t) for t in stockData["t"]]
-    data = {"prices":prices, "dates":dates}
+    data = {"prices": prices, "dates": dates}
     return data
 
-def getQuote(stock: str)->dict:
+
+def getQuote(stock: str) -> dict:
     return IM.getQuote(request.form["name"])
 
-def getViewableName(ticker: str, maxLength = 13)->str:
+
+def getViewableName(ticker: str, maxLength=13) -> str:
     rawName = IM.getName(ticker)
     splitName = rawName.split(" ")
     for i in range(len(splitName)-1):
@@ -45,27 +50,31 @@ def getViewableName(ticker: str, maxLength = 13)->str:
             splitName.pop()
     return " ".join(splitName)
 
-def getCategory(name: str)->list:
+
+def getCategory(name: str) -> list:
     if name in categories.keys():
         return categories.get(name)
     else:
         return categories.get("IT")
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST": #POST
+    if request.method == "POST":  # POST
         # Web Security
         if not WebSecurity.validatePOST(request):
-            return "Bad request. The server could not validate this POST request", 403 #Forbidden Request Status Code
-        
+            # Forbidden Request Status Code
+            return "Bad request. The server could not validate this POST request", 403
+
         # Serve data to the request
         dataQuality = request.form.get("quality", "high")
-        try: #These are put in a try except block in order to prevent crashing when bad post requests are sent
+        try:  # These are put in a try except block in order to prevent crashing when bad post requests are sent
             if request.form["type"] == "quote+candle":
                 if request.form["isMulti"] == 'true':
                     symbolData = {}
                     for symbol in json.loads(request.form["names"]):
-                        symbolData[symbol] = getCandlePlusQuote(symbol, request.form["time"], dataQuality)
+                        symbolData[symbol] = getCandlePlusQuote(
+                            symbol, request.form["time"], dataQuality)
                     return symbolData
                 else:
                     return getCandlePlusQuote(request.form["name"], request.form["time"], dataQuality)
@@ -78,19 +87,21 @@ def index():
 
         except Exception as err:
             print("Internal Server Error: ", err)
-            return "Internal Server Error", 500 #Internal Server Error Status Code
-            
+            return "Internal Server Error", 500  # Internal Server Error Status Code
+
         except KeyError:
             pass
 
-        return "Bad request. The request is missing required keys", 400 #Bad Request Status Code
+        # Bad Request Status Code
+        return "Bad request. The request is missing required keys", 400
 
     else:
         if request.user_agent.platform in mobilePlatforms:
             return redirect("/home?quality=low")
         return redirect("/home?quality=high")
 
-@app.route("/home/", methods = ["GET"])
+
+@app.route("/home/", methods=["GET"])
 def indexWithQuality():
     quality = request.args.get("quality", default="high")
     category = request.args.get("category", default="IT")
@@ -101,14 +112,15 @@ def indexWithQuality():
     response = ""
     workingQuality = quality if quality in ["high", "low"] else "high"
 
-    if len(viewedStocks) > 10:
-        response =  render_template("oneRequestIndex.html", stocks = viewedStocks, quality=workingQuality, stocknames=viewedStockNames)
-    else:
-        response =  render_template("threadedRequestIndex.html", stocks = viewedStocks, quality=workingQuality, delayStep=200, stocknames=viewedStockNames)
+    otherCategories = list(categories.keys())
+    otherCategories.remove(category)
+
+    response = render_template("threadedRequestIndex.html", stocks=viewedStocks,
+                               quality=workingQuality, delayStep=200, stocknames=viewedStockNames, selectedCategory=category, otherCategories = otherCategories)
 
     response = make_response(response)
 
-    #Decide whether this request needs a new sessionID
+    # Decide whether this request needs a new sessionID
     currentSessionID = request.cookies.get('sessionID', default="")
     if not WebSecurity.isSessionStored(currentSessionID):
         response.set_cookie("sessionID", WebSecurity.getNewSessionID())
